5cd66ec0083ce975c447dc38a7b1b69418d88c62
David Jun <djun301@gmail.com>
2021-01-01 04:54:14 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,5 @@
 first_number = "Hello world!".to_i
 second_number = "123".to_i
-def sum("Hello world!", "123")
-  return "Hello world!" + "123"
+def sum(first_number, second_number)
+  return first_number + second_number
 end
