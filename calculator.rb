75518eb444d77aa6ab900bc187f66bacf0d8dff2
David Jun <djun301@gmail.com>
2021-01-01 04:56:52 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,3 +3,4 @@ second_number = "123".to_i
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
+quotient = first_number / second_number
